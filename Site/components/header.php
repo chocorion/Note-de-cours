9e8206584f1a09ded127dbdc115fec5aadc8683c
rob <robiniscoding@gmail.com>
2020-01-15 11:17:00 +0100

    Move in good balise

--- Site/components/header.php
+++ Site/components/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 
-<html>
-    <head <?php echo (isset($overflow))? 'class=overflow': "" ?>
+<html <?php echo (isset($overflow))? 'class=overflow': "" ?>>
+    <head>
         <meta charset="utf-8">
         <title>
             <?php echo (isset($title))? $title : "Cours" ?>
